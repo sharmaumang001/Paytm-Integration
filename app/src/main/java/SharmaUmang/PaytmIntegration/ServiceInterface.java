537973aa74e9e45cbf204411109c92f9cfd2ce78
umang <iumangsharma001@gmail.com>
2020-07-05 02:41:43 +0530

    Full Working app

---
+++ app/src/main/java/SharmaUmang/PaytmIntegration/ServiceInterface.java
@@ -0,0 +1,21 @@
+package SharmaUmang.PaytmIntegration;
+
+
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
+public interface ServiceInterface {
+
+    // method,, return type ,, secondary url
+    @Multipart
+    @POST("paytm_integration/init_Transaction.php")
+    Call<Token_Res> generateTokenCall(
+            @Part("code") RequestBody language,
+            @Part("MID") RequestBody mid,
+            @Part("ORDER_ID") RequestBody order_id,
+            @Part("AMOUNT") RequestBody amount
+    );
+}
